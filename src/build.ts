e56d63aca8a1ce1681c7e8552a771a0d1ba94497
uncenter <47499684+uncenter@users.noreply.github.com>
2025-01-18 12:45:35 -0500

    fix: copy loading spinner assets to build output (#17)

--- src/build.ts
+++ src/build.ts
@@ -1,17 +1,18 @@
 import less from "npm:less";
-import { join, dirname } from "std/path/mod.ts";
+import { join } from "std/path/mod.ts";
 import { flavorEntries } from "npm:@catppuccin/palette@1.1.0";
 
-const THEME_DIR = join(import.meta.dirname as string, "../dist/themes/");
+const SRC_DIR = import.meta.dirname as string;
+const DIST_DIR = join(SRC_DIR, "../dist/");
+const DIST_THEME_DIR = join(DIST_DIR, "themes/");
+const DIST_ASSET_DIR = join(DIST_DIR, "assets/");
 const DEFAULT_ACCENT = "mauve";
 
-await Deno.remove(dirname(THEME_DIR), { recursive: true })
+await Deno.remove(DIST_DIR, { recursive: true })
   .catch(() => {})
-  .finally(() => Deno.mkdir(THEME_DIR, { recursive: true }));
+  .finally(() => Deno.mkdir(DIST_THEME_DIR, { recursive: true }));
 
-const base = await Deno.readTextFile(
-  join(import.meta.dirname as string, "base.less")
-);
+const base = await Deno.readTextFile(join(SRC_DIR, "base.less"));
 
 for (const [flavor, { colorEntries }] of flavorEntries) {
   for (const [accent, { hex }] of colorEntries.filter(
@@ -32,20 +33,33 @@ for (const [flavor, { colorEntries }] of flavorEntries) {
     const { css: withLoader } = await less.render(base, vars);
 
     await Deno.writeTextFile(
-      join(THEME_DIR, `${flavor}-${accent}-no-loader.css`),
+      join(DIST_THEME_DIR, `${flavor}-${accent}-no-loader.css`),
       noLoader
     );
     await Deno.writeTextFile(
-      join(THEME_DIR, `${flavor}-${accent}.css`),
+      join(DIST_THEME_DIR, `${flavor}-${accent}.css`),
       withLoader
     );
 
     if (accent === DEFAULT_ACCENT) {
       await Deno.writeTextFile(
-        join(THEME_DIR, flavor + "-no-loader.css"),
+        join(DIST_THEME_DIR, flavor + "-no-loader.css"),
         noLoader
       );
-      await Deno.writeTextFile(join(THEME_DIR, flavor + ".css"), withLoader);
+      await Deno.writeTextFile(
+        join(DIST_THEME_DIR, flavor + ".css"),
+        withLoader
+      );
     }
   }
+}
+
+await Deno.mkdir(DIST_ASSET_DIR);
+
+const SRC_ASSET_DIR = join(SRC_DIR, "assets/");
+for await (const entry of Deno.readDir(SRC_ASSET_DIR)) {
+  await Deno.copyFile(
+    join(SRC_ASSET_DIR, entry.name),
+    join(DIST_ASSET_DIR, entry.name)
+  );
 }
